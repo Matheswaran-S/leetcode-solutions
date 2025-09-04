f4931cb795fc06bc84b3e1467a3cf9cef22a5572
Matheswaran-S <matheswaran322007@gmail.com>
2025-09-04 22:13:19 +0530

    Time: 3 ms (66.79%) | Memory: 103.8 MB (55.72%) - LeetSync

--- 3244-minimize-length-of-array-using-operations/minimize-length-of-array-using-operations.cpp
+++ 3244-minimize-length-of-array-using-operations/minimize-length-of-array-using-operations.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
     int minimumArrayLength(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        if(nums.size() == 1 || nums[0] != nums[1]) return 1;
-        int tar = nums[0],cnt = 1;
-        for(int i=1; i<nums.size(); i++){
-            if(nums[i] == tar) cnt++;
-            else if(nums[i] != tar && nums[i]%tar != 0) return 1;
+        int n = nums.size();
+        if(n == 1) return 1;
+        int mini = INT_MAX;
+        for(int i=0; i<n; i++) mini = min(mini, nums[i]);
+        int cnt = 0;
+        for(int i=0; i<n; i++){
+            if(nums[i] == mini) cnt++;
+            else if(nums[i]%mini != 0) return 1;
         }
         return (cnt/2)+(cnt%2);
     }
