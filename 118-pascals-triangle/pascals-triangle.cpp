c1a295fba21d3a46844fd2f6e45bf7353b51b657
Matheswaran-S <matheswaran322007@gmail.com>
2025-11-20 00:51:12 +0530

    Time: 1 ms (18.76%) | Memory: 9.7 MB (32.08%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -6,9 +6,7 @@ public:
             vector<int> ans;
             for(int j=0; j<=i; j++){
                 if(j == 0 || j == i) ans.push_back(1);
-                else{
-                    ans.push_back(res[i-1][j-1] + res[i-1][j]);
-                }
+                else ans.push_back(res[i-1][j-1] + res[i-1][j]);
             }
             res.push_back(ans);
         }
