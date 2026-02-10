96da87311d77ca451e40b42a467a66d20ab552a2
Matheswaran-S <matheswaran322007@gmail.com>
2026-02-10 19:58:37 +0530

    Time: 445 ms (7.53%) | Memory: 132.8 MB (9.00%) - LeetSync

--- 204-count-primes/count-primes.cpp
+++ 204-count-primes/count-primes.cpp
@@ -1,19 +1,21 @@
 class Solution {
 private:
-    void sieve(int n, vector<int> &v){
+    int sieve(int n, vector<int> &v, int ans){
         for(int i=2; i<n; i++) v[i] = 1;
         for(int i=2; i*i<n; i++){
             for(int j=i*i; j<n; j += i){
+                if(v[j]) ans--;
                 v[j] = 0;
             }
         }
+        return ans;
     }
 public:
     int countPrimes(int n) {
         vector<int> v(n,0);
-        sieve(n,v);
-        int ans = 0;
+        return max(0,sieve(n,v,n-2));
+        /*int ans = 0;
         for(int i=2; i<n; i++) if(v[i]) ans++;
-        return ans;
+        return ans;*/
     }
 };
