e39e002a4951387575674f5a399c701fea68a570
Matheswaran-S <matheswaran322007@gmail.com>
2026-02-16 21:37:34 +0530

    Time: 168 ms (63.64%) | Memory: 289.5 MB (72.73%) - LeetSync

--- 4217-first-element-with-unique-frequency/first-element-with-unique-frequency.cpp
+++ 4217-first-element-with-unique-frequency/first-element-with-unique-frequency.cpp
@@ -4,12 +4,12 @@ public:
         unordered_map<int,int> mpp;
         int n = nums.size();
         for(int i=0; i<n; i++) mpp[nums[i]]++;
-        unordered_map<int,set<int>> mp;
+        unordered_map<int,int> mp;
         for(auto &[k,v] : mpp){
-            mp[v].insert(k);
+            mp[v]++;
         }
         for(int i=0; i<n; i++){
-            if(mp[mpp[nums[i]]].size() == 1) return nums[i];
+            if(mp[mpp[nums[i]]] == 1) return nums[i];
         }
         return -1;
     }
