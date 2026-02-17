b29bdc3672adca8acb3effdfc11770b1d5a88196
Matheswaran-S <matheswaran322007@gmail.com>
2026-02-17 17:45:40 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (85.28%) - LeetSync

--- 401-binary-watch/binary-watch.cpp
+++ 401-binary-watch/binary-watch.cpp
@@ -1,29 +1,16 @@
 class Solution {
-private:
-    void rec(vector<string> &ans, vector<int> &arr, int cnt, int ind, int hr, int min, int t){
-        if(cnt > t) return;
-        if(hr > 11 || min > 59) return;
-        if(ind == 10){
-            if(cnt == t){
-            string m = "";
-            if(min < 10) m += '0';
-            ans.push_back(to_string(hr)+':'+m+to_string(min));
-            cout<<to_string(hr)+':'+m+to_string(min)<<'\n';
-            return;
-        }
-        }
-        if(ind < 4) hr += arr[ind];
-        else if(ind<10) min += arr[ind];
-        rec(ans, arr, cnt+1, ind+1, hr, min, t);
-        if(ind < 4) hr -= arr[ind];
-        else if(ind < 10) min -= arr[ind];
-        if(ind < 10)rec(ans, arr, cnt, ind+1, hr, min, t);
-    }
 public:
     vector<string> readBinaryWatch(int t) {
-        vector<int> arr = {1,2,4,8,1,2,4,8,16,32};
         vector<string> ans;
-        rec(ans, arr, 0, 0, 0, 0, t);
+        for(int i=0; i<12; i++){
+            for(int j=0; j<60; j++){
+                if(__builtin_popcount(i)+__builtin_popcount(j) == t){
+                    string m =  "";
+                    if(j < 10) m += '0';
+                    ans.push_back(to_string(i)+':'+m+to_string(j));
+                }
+            }
+        }
         return ans;
     }
 };
