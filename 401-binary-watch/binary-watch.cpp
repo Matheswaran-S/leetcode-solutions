1f921fcd7175a003b6cf422a00920dad8ea7f6a2
Matheswaran-S <matheswaran322007@gmail.com>
2026-02-17 17:47:18 +0530

    Time: 3 ms (9.95%) | Memory: 9.6 MB (85.28%) - LeetSync

--- 401-binary-watch/binary-watch.cpp
+++ 401-binary-watch/binary-watch.cpp
@@ -4,6 +4,7 @@ public:
         vector<string> ans;
         for(int i=0; i<12; i++){
             for(int j=0; j<60; j++){
+                if(__builtin_popcount(i) > t) break;
                 if(__builtin_popcount(i)+__builtin_popcount(j) == t){
                     string m =  "";
                     if(j < 10) m += '0';
