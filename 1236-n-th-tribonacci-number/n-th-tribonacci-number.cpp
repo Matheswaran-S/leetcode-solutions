4338c4c1503ebcd1d38ec19976d57d12f69a3fea
Matheswaran-S <matheswaran322007@gmail.com>
2026-02-19 22:24:01 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (67.05%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
@@ -5,10 +5,8 @@ public:
         if(n<=1) return n;
         for(int i=3; i<=n; i++){
             t0 = t0+t1+t2;
-            //cout<<t0<<endl;
             swap(t0,t2);
             swap(t2,t1);
-            cout<<t0<<' '<<t1<<' '<<t2<<endl;
         }
         return t1;
     }
