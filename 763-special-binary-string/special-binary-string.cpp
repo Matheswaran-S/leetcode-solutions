aa5842abb3f6fb026e3eb01e37d6268dfe3017cf
Matheswaran-S <matheswaran322007@gmail.com>
2026-02-20 22:27:31 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (6.88%) - LeetSync

--- 763-special-binary-string/special-binary-string.cpp
+++ 763-special-binary-string/special-binary-string.cpp
@@ -1,23 +1,25 @@
 class Solution {
 private:
-    string rec(int l, int r, string &s){
+    string rec(int l, int r, string &s, map<string,string> &mpp){
         if(l == r) return "";
+        if(mpp[s.substr(l,r-l+1)] != "") return mpp[s.substr(l,r-l+1)];
         vector<string> res;
-        int cnt = 0;
+        int cnt = 0, j = l;
         for(int i=l; i<=r; i++){
             cnt += s[i] == '1' ? 1 : -1;
             if(cnt == 0){
-                res.push_back('1'+rec(l+1, i-1, s)+'0');
+                res.push_back('1'+rec(l+1, i-1, s, mpp)+'0');
                 l = i+1;
             }
         }
         sort(res.rbegin(), res.rend());
         string t = "";
         for(auto &i : res) t += i;
-        return t;
+        return mpp[s.substr(j,r-j+1)] = t;
     }
 public:
     string makeLargestSpecial(string s) {
-        return rec(0,s.size()-1, s);
+        map<string,string> mpp;
+        return rec(0,s.size()-1, s, mpp);
     }
 };
