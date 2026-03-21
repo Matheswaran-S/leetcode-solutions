f3f7c48ee19e63c130ba56699975abcd1a0bb831
Matheswaran-S <matheswaran322007@gmail.com>
2026-03-21 17:30:31 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (49.53%) - LeetSync

--- 1130-last-stone-weight-ii/last-stone-weight-ii.cpp
+++ 1130-last-stone-weight-ii/last-stone-weight-ii.cpp
@@ -1,32 +1,20 @@
 class Solution {
 public:
-    int rec(vector<int> &stones, int ind, int sum, vector<vector<int>> dp){
-        if(ind == 0){
-            if(sum - 2*stones[0] >= 0) return sum-2*stones[0];
-            else return sum;
-        }
-        if(dp[ind][sum] != -1) return dp[ind][sum];
-        int nt = rec(stones, ind-1, sum, dp);
-        int t = INT_MAX;
-        if(sum >= 2*stones[ind]) t = rec(stones, ind-1, sum - 2*stones[ind], dp);
-        return dp[ind][sum] = min(t,nt);
-    }
     int lastStoneWeightII(vector<int>& stones) {
         int n = stones.size();
         int sum = accumulate(stones.begin(), stones.end(), 0);
-        vector<int> prev(sum+1, 0), cur(sum+1, 0);
+        vector<int> prev(sum+1, 0);
         for(int i=0; i<=sum; i++){
             if(i >= 2*stones[0]) prev[i] = i-2*stones[0];
             else prev[i] = i;
         }
         for(int i=1; i<n; i++){
-            for(int j=0; j<=sum; j++){
+            for(int j=sum; j>=0; j--){
                 int nt = prev[j];
                 int t = INT_MAX;
                 if(j >= 2*stones[i]) t = prev[j - 2*stones[i]];
-                cur[j] = min(t,nt);
+                prev[j] = min(t,nt);
             }
-            prev = cur;
         }
         return prev[sum];
     }
