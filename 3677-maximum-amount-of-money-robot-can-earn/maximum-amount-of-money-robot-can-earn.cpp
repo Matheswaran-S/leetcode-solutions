f04f678fe91813813cf23380d66b928c63ddfaee
Matheswaran-S <matheswaran322007@gmail.com>
2026-04-02 12:47:50 +0530

    Time: 556 ms (50.64%) | Memory: 317 MB (5.53%) - LeetSync

--- 3677-maximum-amount-of-money-robot-can-earn/maximum-amount-of-money-robot-can-earn.cpp
+++ 3677-maximum-amount-of-money-robot-can-earn/maximum-amount-of-money-robot-can-earn.cpp
@@ -1,36 +1,4 @@
 class Solution {
-private:
-    long long dfs(vector<vector<int>> &coins, int i, int j, int nor, vector<vector<vector<long long>>> dp){
-        if(i<0 || j<0 || i>=coins.size() || j>=coins[0].size()){
-            return INT_MIN;
-        }
-        if(i==0 && j==0){
-            if(coins[i][j] < 0){
-                if(nor){
-                    nor--;
-                    return dp[i][j][nor] = 0;
-                }
-                else return dp[i][j][nor] = coins[i][j];
-            }
-            else return dp[i][j][nor] = coins[i][j];
-        }
-        if(dp[i][j][nor] != LLONG_MIN) return dp[i][j][nor];
-        if(coins[i][j] >= 0){
-            return dp[i][j][nor] = max(coins[i][j]+dfs(coins, i-1, j, nor, dp), coins[i][j]+dfs(coins, i, j-1, nor, dp));
-        }
-        else{
-            if(nor == 0){
-                return dp[i][j][nor] = max(coins[i][j]+dfs(coins, i-1, j, nor, dp), coins[i][j]+dfs(coins, i, j-1, nor, dp));
-            }
-            else{
-                long long t1 = dfs(coins, i, j-1, nor-1, dp);
-                long long t2 = dfs(coins, i-1, j, nor-1, dp);
-                long long t3 = coins[i][j]+dfs(coins, i, j-1, nor, dp);
-                long long t4 = coins[i][j]+dfs(coins, i-1, j, nor, dp);
-                return dp[i][j][nor] = max(max(t1,t2),max(t3,t4));
-            }
-        }
-    }
 public:
     int maximumAmount(vector<vector<int>>& coins) {
         int m = coins.size(), n = coins[0].size();
