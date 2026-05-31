20ba324431b593a426c791d9f8c0da5b7acc92c1
Matheswaran-S <matheswaran322007@gmail.com>
2026-05-31 11:57:47 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (83.23%) - LeetSync

---
+++ 4321-digit-frequency-score/digit-frequency-score.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int digitFrequencyScore(int n) {
+        int s = 0;
+        while(n){
+            s += n%10;
+            n = n/10;
+        }
+        return s;
+    }
+};
