ecc1b89c5f274eda6194a04ab7c6edf8346f9617
Matheswaran S <matheswaran322007@gmail.com>
2026-08-15 22:22:42 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (78.31%) - LeetSync

--- 4331-k-th-digit-in-infinite-string/k-th-digit-in-infinite-string.cpp
+++ 4331-k-th-digit-in-infinite-string/k-th-digit-in-infinite-string.cpp
@@ -4,18 +4,14 @@ class Solution {
 public:
     int kthDigit(long long k) {
         if (k <= 9) return k;
-
         ll pref = 9, nine = 9, dig = 2, act = 9;
-
         while (pref + dig * nine * 10 < k) {
             nine *= 10;
             pref += dig * nine;
             dig++;
             act = act*10 + 9;
         }
-
         bool rev;
-
         if (((k - pref - 1) / (dig * 10)) & 1) {
             if (dig == 2)
                 rev = false;
@@ -28,7 +24,6 @@ public:
             else
                 rev = false;
         }
-
         ll rem = k - pref-1;
         ll num = act + (rem/dig) + 1;
         if(rev) num = (num/10)*10 + (9 - num%10);
