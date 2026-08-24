37cafbc1dd5697eacba9f39b1c83755e50feffa5
Matheswaran S <matheswaran322007@gmail.com>
2026-08-24 18:49:53 +0530

    Time: 106 ms (60.76%) | Memory: 95.4 MB (41.00%) - LeetSync

--- 2002-stone-game-viii/stone-game-viii.cpp
+++ 2002-stone-game-viii/stone-game-viii.cpp
@@ -1,18 +1,13 @@
 class Solution {
 public:
-    int f(int i, vector<int> &pre, int n, unordered_map<int,int> &dp){
-        if(i == n-1) return pre[i];
-        if(dp.find(i) != dp.end()) return dp[i];
-        int take = pre[i] - f(i+1, pre, n, dp);
-        int nt = f(i+1, pre, n, dp);
-        return dp[i] = max(take, nt);
-    }
     int stoneGameVIII(vector<int>& nums) {
         int n = nums.size();
         vector<int> pre(n, 0);
         pre[0] = nums[0];
         for(int i=1; i<n; i++) pre[i] = pre[i-1]+nums[i];
-        unordered_map<int,int> dp;
-        return f(1,pre,n, dp);
+        vector<int> dp(n);
+        dp[n-1] = pre[n-1];
+        for(int i=n-2; i>=1; i--) dp[i] = max(dp[i+1], pre[i]-dp[i+1]);
+        return dp[1];
     }
 };
